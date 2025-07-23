b401d41b8d2ed8ee1b0086e1d04a221070be7096
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-07-23 18:10:36 +0530

    Clerk Billing

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import { PricingTable } from "@clerk/nextjs";
+import React from "react";
 
 function page() {
-    return (
-        <div>
-            Subscription
-        </div>
-    )
+  return (
+    <div>
+      <PricingTable />
+    </div>
+  );
 }
 
-export default page
+export default page;
